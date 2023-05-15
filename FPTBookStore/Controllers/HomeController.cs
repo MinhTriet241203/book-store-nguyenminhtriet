5bd15e34237a9cd8faf6b2814cacc512f81f2591
Nguyễn Minh Triết <minhtriet241203@gmail.com>
2023-05-15 19:53:54 +0700

    Here we go again

--- FPTBookStore/Controllers/HomeController.cs
+++ FPTBookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using FPTBookStore.Models;
+﻿using AutoEmailingServices.Models;
+using AutoEmailingServices.Services;
+using FPTBookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,18 +9,26 @@ namespace FPTBookStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IEmailService _emailService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+            IEmailService emailService //todo: remove this
+            )
         {
             _logger = logger;
+            _emailService = emailService; //and this
         }
 
         public IActionResult Index()
         {
             return View();
         }
+		public IActionResult HomePage()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
+		public IActionResult Shop()
         {
             return View();
         }
@@ -27,6 +37,17 @@ namespace FPTBookStore.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //demo only -- TODO: remove this
+        [HttpGet]
+        public IActionResult SendEmail()
+        {
+            var message = new Message(new string[] { "mrshine4k@gmail.com", "trietnmgcs210026@fpt.edu.vn" },
+                "Email từ app thư viện",
+                "Gửi được email thư viện rồi nè :O https://i.imgflip.com/5c15oj.png?a467760");
+            _emailService.SendEmail(message);
+            return StatusCode(StatusCodes.Status200OK);
         }
     }
 }
