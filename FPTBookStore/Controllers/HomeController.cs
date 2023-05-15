0610ff09478e9c34602462a3c00af86ed065f8db
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2023-05-15 20:16:56 +0700

    Identity scaffolding

--- FPTBookStore/Controllers/HomeController.cs
+++ FPTBookStore/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace FPTBookStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        //private readonly IEmailService _emailService;
+        private readonly IEmailService _emailService;
         public HomeController(ILogger<HomeController> logger, IEmailService emailService)
         {
             _logger = logger;
